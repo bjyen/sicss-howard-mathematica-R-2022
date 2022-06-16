170c47a4eba43a9602804b4e2d5fd545bf848c4c
Evan Muzzall <muzzall@stanford.edu>
2022-06-16 04:40:53 -0700

    Update install.R

--- install.R
+++ install.R
@@ -2,6 +2,6 @@ install.packages(c("tidyverse",               # tidyr, readr, dplyr, ggplot2
                    "psych",                   # describe() and describeBy() summary statistics functions
                    "kableExtra", "flextable", # for making reproducible tables
                    "gapminder",               # gapminder dataset
-                   "ggtheme", "ggrepel",      # additional themes for ggplot2; avoid text overlapping
+                   "ggthemes", "ggrepel",      # additional themes for ggplot2; avoid text overlapping
                    # "patchwork",             # make compound figures
                    "broom"))                  # for statistical modeling
